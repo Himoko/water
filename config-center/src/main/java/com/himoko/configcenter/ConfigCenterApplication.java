5db48fa6b1a597f2846d06cd531116484f1bde70
himoko <peng.zhang11@ucarinc.com>
2019-11-01 11:36:48 +0800

    first commit

---
+++ config-center/src/main/java/com/himoko/configcenter/ConfigCenterApplication.java
@@ -0,0 +1,13 @@
+package com.himoko.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class, args);
+    }
+
+}
