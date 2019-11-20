1b2a4407b57dd0f8b3919c08d208fed4dce63215
himoko <peng.zhang11@ucarinc.com>
2019-11-20 16:39:08 +0800

    开启VUE gzip打包 使用history模式

--- water-fe/config/index.js
+++ water-fe/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-          target: 'http://localhost:8080',
+          target: 'http://127.0.0.1:9009',
           changeOrigin: true,
           pathRewrite: {
             '^//api': ''
@@ -72,7 +72,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
--- water-fe/src/router/index.js
+++ water-fe/src/router/index.js
@@ -5,7 +5,7 @@ import Test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
